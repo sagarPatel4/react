33e5332f378456ae12b7bef41f61a6698f74e291
Sagar Patel <sagarpatel04sr@gmail.com>
2024-10-13 17:37:10 +0530

    add new code

--- 01viteReactProject/src/App.jsx
+++ 01viteReactProject/src/App.jsx
@@ -1,9 +1,51 @@
+import { useState } from "react";
+
 function App() {
 
+  let [counter, setCounter] = useState(15)
+  let [msg, setMsg] = useState("")
+
+  const addValue = () => {
+
+    console.log("conter" + counter);
+    counter = counter + 1
+
+    if (counter <= 20) {
+      setCounter(counter)
+      setMsg("")
+    } else {
+      counter = 20
+      setMsg("you can not add more than 20 number")
+    }
+
+  }
+
+  const removeValue = () => {
+
+    console.log("removevalue" + msg);
+    counter = counter - 1
+
+    if (counter > 0) {
+      setCounter(counter)
+      setMsg("")
+    } else {
+      counter = 0
+      setMsg("not possible to minus from zero")
+    }
+
+  }
   return (
-    <h1>
-      chai aur react | sagar
-    </h1>
+    <>
+      <h1>chai aur react | sagar</h1>
+      <h2>Counter value :{counter} {msg}</h2>
+      <h2>msg:{msg}</h2>
+
+      <button onClick={addValue}>Add value</button>
+
+      <br />
+      <button onClick={removeValue}>remove value</button>
+    </>
+
   )
 }
 
